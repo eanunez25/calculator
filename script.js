13b607a28f2fde263ff7e975dd3caff6a0f0c802
Elvin Nunez <eanunez25@gmail.com>
2020-04-21 00:23:18 -0500

    Passable, still has a few bugs

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ nums.forEach(num => num.addEventListener("mouseup", e => {
 }));
 
 const inputScreen = document.querySelector(".input");
+const outputScreen = document.querySelector(".output");
 onScreenNum = x => {
     window.input = x.currentTarget.innerHTML;
     if (input == "+/-") {
         inputScreen.innerHTML += "";
     } else if (input == "C") {
         inputScreen.innerHTML = "";
+        outputScreen.innerHTML = "";
     } else if (input == "del") {
         delLastEntry();
     } else if (input == "=") {
@@ -21,7 +23,28 @@ onScreenNum = x => {
 
 getNumInput = () => {
     const inputBox = document.querySelector(".input").innerHTML;
-    const inputArray = inputBox.split(/\D+/);
+    const inputArray = inputBox.split(/[/x+-]+/);
+    const array = inputBox.split("");
+    const operator = array.filter(op => {
+        return (op == "/" || op == "x" || op == "+" || op == "-")
+    })
+    if (operator == "/") {
+        const quotient = inputArray[0] / inputArray[1];
+        outputScreen.innerHTML = lessDecimals(quotient);
+        inputScreen.innerHTML = quotient;
+    } else if (operator == "x") {
+        const product = inputArray[0] * inputArray[1];
+        outputScreen.innerHTML = lessDecimals(product);
+        inputScreen.innerHTML = product;
+    } else if (operator == "+") {
+        const sum = inputArray[0] + inputArray[1];
+        outputScreen.innerHTML = lessDecimals(sum);
+        inputScreen.innerHTML = sum;
+    } else {
+        const difference = inputArray[0] - inputArray[1];
+        outputScreen.innerHTML = lessDecimals(difference);
+        inputScreen.innerHTML = difference;
+    }
 }
 
 delLastEntry = () => {
@@ -30,6 +53,7 @@ delLastEntry = () => {
     inputArray.pop()
     const returnString = inputArray.join("");
     inputScreen.innerHTML = returnString;
+    outputScreen.innerHTML = "";
 }
 
 onlyOneOp = () => {
@@ -41,11 +65,17 @@ onlyOneOp = () => {
             opCounter++;
         }
     }
-    if (opCounter > 1) {
-        inputScreen.innerHTML += "";
-    } else {
+    if (opCounter < 2) {
         inputScreen.innerHTML += input;
+    } else {
+        inputScreen.innerHTML += "";
     }
 }
 
+lessDecimals = (int) => {
+    if (int.toString().split(".")[1] > 4) {
+        int = int.toFixed(4)
+        return int
+    }
+}
 
